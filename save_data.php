eae1e0f890162a501e6fb9b5835648160d1ba571
haz4141 <124134377+haz4141@users.noreply.github.com>
2024-12-07 18:13:38 +0800

    Update save_data.php

--- save_data.php
+++ save_data.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user_input = $_POST["user_input"];
     file_put_contents("data.txt", $user_input . "\n", FILE_APPEND);
     
-    // Redirect to bye page after submission
+    // Redirect to bye.html after submission
     header("Location: bye.html");
     exit();
 }
